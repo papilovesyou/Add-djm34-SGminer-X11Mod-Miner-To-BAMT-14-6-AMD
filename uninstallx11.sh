523ee39d581d91d74210a8f19b1d71cbb50b7fc9
papilovesyou <chris.cas22@gmail.com>
2014-07-22 19:39:16 -0400

    Update uninstallx11.sh

--- uninstallx11.sh
+++ uninstallx11.sh
@@ -5,7 +5,7 @@ cp common.unpatch.diff /tmp/
 cd /opt/miners/
 rm -rf sgminer-x11mod
 cd /etc/bamt/
-mv sgminer-x11mod.conf sgminer-x11mod.conf.bak
+mv djm34-x11mod.conf djm34-x11mod.conf.bak
 cd /opt/bamt/
 cp /tmp/common.unpatch.diff .
 sleep 2
@@ -13,7 +13,7 @@ patch common.pl < common.unpatch.diff
 rm common.unpatch.diff
 cd /etc/bamt/
 echo 'Please remove the following lines from bamt.conf manually'
-echo '#cgminer_opts: --api-listen --config /etc/bamt/sgminer-x11mod.conf'
-echo '# Sgminer X11 Mod "DRK"'
-echo 'miner-sgminer-x11mod: 1'
+echo '#cgminer_opts: --api-listen --config /etc/bamt/djm34-x11mod.conf'
+echo '# djm34 Sgminer X11 Mod "DRK"'
+echo 'miner-djm34-x11mod: 1'
 echo 'Done'
